1137b391a90ee2fc7d3ed25b4217d456b6e89067
Ro98h <52565633+Ro98h@users.noreply.github.com>
2021-04-30 14:41:11 +0200

    Update Rank.java

--- src/Rank.java
+++ src/Rank.java
@@ -1,7 +1,6 @@
 // link on GitHub
 //https://github.com/Ro98h/Cards.git
 
-//need help with git
 public enum Rank {
     ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN,
     EIGHT, NINE, TEN, JACK, QUEEN, KING
